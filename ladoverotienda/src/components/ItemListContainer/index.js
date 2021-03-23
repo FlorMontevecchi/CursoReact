0d877b9f8f868138d96dee849fe27a455b2066cc
Flor Montevecchi <florgvs@gmail.com>
2021-03-23 19:46:06 -0300

    resubida

---
+++ ladoverotienda/src/components/ItemListContainer/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { ItemCount } from "../ItemCount";
+import {ItemList} from "../ItemList"
+
+export default function ItemListContainer() {
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+
+    const prom = new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve([
+          { id:1, title:"Helsinsky", price:560, pictureUrl:"https://cdn.pixabay.com/photo/2021/01/06/10/11/burger-5893927_960_720.jpg" },
+          { id:2, title:"Nairobi", price:450, pictureUrl:"https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg"},
+          { id:3, title:"Berlin", price:320, pictureUrl:"https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_1280.jpg" },
+          { id:3, title:"Tokio", price:530, pictureUrl:"https://cdn.pixabay.com/photo/2017/08/02/22/11/burger-2573682_1280.jpg" }
+        ])
+      },2000)
+    })
+
+    prom.then((resultado)=>{
+      setItems(resultado)
+    })
+
+  })
+
+  return (
+    <div className="container ">
+      
+      <ItemList items={items}/>
+      <ItemCount stock="6" initial="2" />
+    </div>
+  );
+}
---
+++ ladoverotienda/src/components/NavBar/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import CartWidget from "../CartWidget";
+
+export const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#Home">
+        Ladovero Tienda
+      </a>
+
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#Home">
+            Hamburguesas
+          </a>
+          <a className="nav-item nav-link" href="#Courses">
+            Empanadas
+          </a>
+          <a className="nav-item nav-link" href="#Consulting">
+            Pizzas
+          </a>
+        </div>
+      </div>
+      <CartWidget />
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  );
+};
